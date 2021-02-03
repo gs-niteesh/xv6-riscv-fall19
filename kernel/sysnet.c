308c5306cea2ec27fe90cc784a6163f46525c372
G S Niteesh Babu <niteesh.gs@gmail.com>
2021-02-03 12:20:35 +0530

    Lab net finished

--- kernel/sysnet.c
+++ kernel/sysnet.c
@@ -86,17 +86,118 @@ bad:
 // Add and wire in methods to handle closing, reading,
 // and writing for network sockets.
 //
+int
+sockread(struct sock *sock, uint64 addr, int n)
+{
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+  struct mbuf *pckt;
+  struct proc *p;
+
+  p = myproc();
+
+  acquire(&sock->lock);
+  if(mbufq_empty(&sock->rxq)) {
+    sleep(sock, &sock->lock);
+  }
+
+  pckt = mbufq_pophead(&sock->rxq);
+  release(&sock->lock);
+
+  if(!pckt)
+    panic("sockread: buf null\n");
+
+  n = MIN(n, pckt->len);
+
+  if(copyout(p->pagetable, addr, (char *)pckt->head, n) != 0){
+    return -1;
+  }
+
+  mbuffree(pckt);
+
+  return n;
+}
+
+int
+sockwrite(struct sock *sock, uint64 addr, int n)
+{
+  struct mbuf *pckt;
+  struct proc *p;
+
+  // Allocated space for IP, ETH, ARP, UDP headers
+  if((pckt = mbufalloc(70)) == 0)
+    return -1;
+
+  p = myproc();
+
+  if(copyin(p->pagetable, pckt->head, addr, n) != 0){
+    return -1;
+  }
+
+  pckt->len = n;
+
+  net_tx_udp(pckt, sock->raddr, sock->lport, sock->rport);
+  return 0;
+}
+
+void
+sockclose(struct sock *sock)
+{
+  struct mbuf *buf;
+  struct sock *iter;
+
+  // clear all mbufs
+  acquire(&sock->lock);
+  buf = sock->rxq.head;
+  while(buf){
+    struct mbuf *next = buf->next;
+    mbuffree(buf);
+    buf = next;
+  }
+  release(&sock->lock);
+
+  // remove the sock object from the socket list
+  acquire(&lock);
+  iter = sockets;
+  while(iter){
+    if(iter->next == sock) {
+      iter->next = sock->next;
+      break;
+    }
+    iter = iter->next;
+  }
+
+  kfree(sock);
+  if(sock == sockets)
+    sockets = 0;
+  release(&lock);
+}
 
 // called by protocol handler layer to deliver UDP packets
 void
 sockrecvudp(struct mbuf *m, uint32 raddr, uint16 lport, uint16 rport)
 {
-  //
-  // Your code here.
-  //
-  // Find the socket that handles this mbuf and deliver it, waking
-  // any sleeping reader. Free the mbuf if there are no sockets
-  // registered to handle it.
-  //
-  mbuffree(m);
+  struct sock *sock = sockets;
+  int found = 0;
+
+  // Find the corresponding socket for the received UDP msg
+  acquire(&lock);
+  while(sock) {
+    if(sock->raddr == raddr && sock->lport == lport && sock->rport == rport){
+      found = 1;
+      break;
+    }
+    sock = sock->next;
+  }
+  release(&lock);
+
+  // Free mbuf if there is no socket to receive
+  if(!found){
+    mbuffree(m);
+    return ;
+  }
+
+  acquire(&sock->lock);
+  mbufq_pushtail(&sock->rxq, m);
+  release(&sock->lock);
+  wakeup(sock);
 }
